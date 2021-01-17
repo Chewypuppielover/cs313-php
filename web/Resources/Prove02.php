5e21eca437df6e3cd0158b8c187ac8b71d66dd3f
Suzy <chewypuppielover@yahoo.com>
2021-01-16 20:39:20 -0700

    Prove 2 and indexes

---
+++ web/Resources/Prove02.php
@@ -0,0 +1,37 @@
+<html>
+   <head>
+      <meta charset = "utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>Character</title>
+      <link rel="stylesheet" href="resources/Prove02.css">
+   </head>
+   
+   <body>
+      <header>
+         <?php print $_GET['CharacterName']; ?>
+      </header>
+      <div class="flex">
+         <img src="https://cdnb.artstation.com/p/assets/images/images/006/017/293/large/kevin-furr-halfelfranger.jpg?1495455953" 
+             alt="Dungeons and Dragons Character" width="325" height="600">
+      </div>
+      <div>
+         <div class="col1">
+            <?php print $_GET['gender']; ?>
+         </div>
+         <div class="col2">
+            <?php print $_GET['align'] $_GET['align1']; ?>
+         </div>
+         <div class="col3">
+            <?php print $_GET['Class']; ?>
+         </div>
+         <div class="col4">
+            <?php print $_GET['Race']; ?>
+         </div>
+      </div>
+      <div>
+         <?php print $_GET['Ideal']; ?>
+         <?php print $_GET['Bond']; ?>
+         <?php print $_GET['Flaw']; ?>
+      </div>
+   </body>
+</html>
--- web/index.php
+++ web/index.php
@@ -1,5 +1,19 @@
+<!DOCTYPE html>
+   <head>
+      <meta charset = "utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title> Suzy Naegle's Heroku Index </title>
+   </head>
+   <body>
+      <div>
+         <a href="Prove02.html">    Home Page </a> <br>
+         <a href="indexProve.html"> Assignments </a> <br>
+         <a href="indexTeam.html"> Team Activities </a> <br>
+      </div>
+      <hr/>
+   </body>
+</html>
+
 <?php
-
   phpinfo();
-
 ?>
