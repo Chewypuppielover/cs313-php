d4f26529f6acd1fb9690c27020b55006662ccea5
Suzy <chewypuppielover@yahoo.com>
2021-01-16 21:21:59 -0700

    Cosmetic changes

--- web/Resources/Prove02.php
+++ web/Resources/Prove02.php
@@ -7,30 +7,32 @@
    </head>
    
    <body>
-      <header>
+      <div class="name">
          <?php print $_GET['CharacterName']; ?>
-      </header>
+      </div>
       <div class="flex">
          <img src="https://cdnb.artstation.com/p/assets/images/images/006/017/293/large/kevin-furr-halfelfranger.jpg?1495455953" 
              alt="Dungeons and Dragons Character" width="160" height="300">
-         <div class="col1">
-            <?php print $_GET['gender']; ?>
+         <div >
+            <div class="col1">
+               <?php print $_GET['gender']; ?>
+            </div>
+            <div class="col2">
+               <?php print $_GET['align'];
+                     print $_GET['align1']; ?>
+            </div>
+            <div class="col3">
+               <?php print $_GET['Class']; ?>
+            </div>
+            <div class="col4">
+               <?php print $_GET['Race']; ?>
+            </div>
          </div>
-         <div class="col2">
-            <?php print $_GET['align'];
-                  print $_GET['align1']; ?>
+         <div>
+            <?php print $_GET['Ideal']; ?>
+            <?php print $_GET['Bond']; ?>
+            <?php print $_GET['Flaw']; ?>
          </div>
-         <div class="col3">
-            <?php print $_GET['Class']; ?>
-         </div>
-         <div class="col4">
-            <?php print $_GET['Race']; ?>
-         </div>
-      </div>
-      <div>
-         <?php print $_GET['Ideal']; ?>
-         <?php print $_GET['Bond']; ?>
-         <?php print $_GET['Flaw']; ?>
       </div>
    </body>
 </html>
