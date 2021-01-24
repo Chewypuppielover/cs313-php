24082a2da7c9ea31b33ca0932e8ca0a99c8d8ec1
Suzy <chewypuppielover@yahoo.com>
2021-01-23 23:33:13 -0700

    why

--- web/Resources/Prove03_checkout.php
+++ web/Resources/Prove03_checkout.php
@@ -22,6 +22,12 @@
             xhttp.send();
          }
       </script>
+      <style>
+         .h3 {
+            font-size: 1.17em;
+            font-weight: bold;
+         }
+      </style>
    </head>
    <body>
       <div id="DEBUG" style="display:none">
@@ -40,7 +46,7 @@
       </header>
       <hr/>
       <div id=cart>
-         <h3>Products in Cart</h3>
+         <p class=h3>Products in Cart</p>
          <?php
             $count = 0;
             foreach($_SESSION["cart"] as $item => $x) {
@@ -51,6 +57,7 @@
             }
             if($count == 0) echo "<h4>No items in Cart</h4>";
          ?>
+         </br>
       </div>
       <div id="main">
          <form method="post" onsubmit="AJAX();return false;">
@@ -58,23 +65,26 @@
             <label> Apt #: <input type="text" name="apt" size="2"></label> </br>
             <label>City: <input type="text" name="city"></label>
             <label> State: <input type="text" name="state"></label>
-            <label> Zip Code: <input type="text" name="zip" size="4"></label>
+            <label> Zip Code: <input type="text" name="zip" size="4"></label> </br>
             <input type="submit" name="End_Session" value="Finalize">
          </form>
       </div>
-      <?php
-         if(isset($_POST["End_Session"])) {
-            echo $_POST["street"] . " " . $_POST["apt"] . "</br>" . $_POST["city"] . ", " . $_POST["state"] . ", " . $_POST["zip"];
-            if($_SESSION["DEBUG"]) {
-               $_SESSION['DBCOUNT'] += 1;
-               echo "ending session " . $_SESSION['DBCOUNT'] . "<br>";
+      <div>
+         <?php
+            echo "Debug Div";
+            if(isset($_POST["End_Session"])) {
+               echo $_POST["street"] . " " . $_POST["apt"] . "</br>" . $_POST["city"] . ", " . $_POST["state"] . ", " . $_POST["zip"];
+               if($_SESSION["DEBUG"]) {
+                  $_SESSION['DBCOUNT'] += 1;
+                  echo "ending session " . $_SESSION['DBCOUNT'] . "<br>";
+               }
+               unset($_POST["End_Session"]);
+               unset($_SESSION["cart"]);
+                  if($_SESSION["DEBUG"]) print_r($_SESSION);
+               header("Refresh:0");
+               //session_unset(); session_destroy(); session_start();
             }
-            unset($_POST["End_Session"]);
-            unset($_SESSION["cart"]);
-               if($_SESSION["DEBUG"]) print_r($_SESSION);
-            header("Refresh:0");
-            //session_unset(); session_destroy(); session_start();
-         }
-      ?>
+         ?>
+      </div>
    </body>
 </html>
