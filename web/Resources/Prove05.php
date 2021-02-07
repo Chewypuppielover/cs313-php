852d479b277249e0a004082b00c0e24b92ac5ec5
Suzy <chewypuppielover@yahoo.com>
2021-02-06 21:43:30 -0700

    PHP is soooo annoying

--- web/Resources/Prove05.php
+++ web/Resources/Prove05.php
@@ -2,7 +2,7 @@
    require('DBconnect.php');
    $db = get_db();
    
-   var $stm = "SELECT sc.name AS school, so.name AS source, ct.name AS casting_time_type, d.name AS duration_type, s.name AS save_attack, spells.name, spells.casting_time, spells.duration, spells.lvl, spells.concentration, spells.ritual, spells.range, spells.range_type, spells.components, spells.component_desc, spells.consumed, spells.description, spells.higher_desc, spells.save_id " .
+   $stm = "SELECT sc.name AS school, so.name AS source, ct.name AS casting_time_type, d.name AS duration_type, s.name AS save_attack, spells.name, spells.casting_time, spells.duration, spells.lvl, spells.concentration, spells.ritual, spells.range, spells.range_type, spells.components, spells.component_desc, spells.consumed, spells.description, spells.higher_desc, spells.save_id " .
       "FROM project1.spells AS spells, project1.schools AS sc, project1.sources AS so, project1.lengths AS ct, project1.lengths AS d, project1.saves_attacks AS s " .
       "WHERE spells.school_id = sc.id " .
       "AND spells.source_id = so.id " .
