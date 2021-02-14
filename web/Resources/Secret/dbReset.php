df9d1a088cb8e650f48480a59b4994262b206549
Suzy <chewypuppielover@yahoo.com>
2021-02-14 14:42:11 -0700

    that's annoying

--- web/Resources/Secret/dbReset.php
+++ web/Resources/Secret/dbReset.php
@@ -1,79 +1,58 @@
 <?php
-   $delete = 'DROP TABLE IF EXISTS project1.spells_by_class,
-      project1.spells, project1.saves_attacks, project1.lengths,
-      project1.classes, project1.schools, project1.sources, project1.users';
-   $create = "CREATE TABLE project1.users
-      (
-         id SERIAL NOT NULL UNIQUE PRIMARY KEY,
-         username VARCHAR(100) NOT NULL UNIQUE,
-         password VARCHAR(100) NOT NULL,
-         name VARCHAR(100) NOT NULL
-      );
-      CREATE TABLE project1.sources
-      (
-         id SERIAL NOT NULL PRIMARY KEY,
-         name VARCHAR(100) NOT NULL UNIQUE
-      );
-      CREATE TABLE project1.schools
-      (
-         id SERIAL NOT NULL UNIQUE PRIMARY KEY,
-         name VARCHAR(100) NOT NULL UNIQUE
-      );
-      CREATE TABLE project1.classes
-      (
-         id SERIAL NOT NULL UNIQUE PRIMARY KEY,
-         name VARCHAR(100) NOT NULL UNIQUE
-      );
-      CREATE TABLE project1.lengths
-      (
-         id SERIAL NOT NULL UNIQUE PRIMARY KEY,
-         name VARCHAR(100) NOT NULL UNIQUE
-      );
-      CREATE TABLE project1.saves_attacks
-      (
-         id SERIAL NOT NULL UNIQUE PRIMARY KEY,
-         name VARCHAR(100) NOT NULL UNIQUE
-      );
-      CREATE TABLE project1.spells
-      (
-         id SERIAL NOT NULL UNIQUE PRIMARY KEY,
-         name VARCHAR(100) NOT NULL UNIQUE,
-         school_id INT NOT NULL REFERENCES project1.schools(id),
-         source_id INT NOT NULL REFERENCES project1.sources(id),
-         casting_time_id INT NOT NULL REFERENCES project1.lengths(id),
-         duration_id INT NOT NULL REFERENCES project1.lengths(id),
-         save_id INT REFERENCES project1.saves_attacks(id),
-         casting_time INT NOT NULL,
-         duration DECIMAL NOT NULL,
-         lvl SMALLINT NOT NULL, -- only need numbers -1 thru 10
-         concentration BOOLEAN NOT NULL,
-         ritual BOOLEAN NOT NULL,
-         range INT,
-         range_type VARCHAR(30),
-         area VARCHAR(100),
-         components VARCHAR(7) NOT NULL,
-         component_desc VARCHAR(100),
-         consumed BOOLEAN NOT NULL,
-         description TEXT NOT NULL,
-         higher_desc TEXT
-      );
-      CREATE TABLE project1.spells_by_class
-      (
-         id SERIAL NOT NULL UNIQUE PRIMARY KEY,
-         class_id INT NOT NULL REFERENCES project1.classes(id),
-         spell_id INT NOT NULL REFERENCES project1.spells(id)
-      )";
-   
    if($_SERVER['REQUEST_METHOD'] == 'POST') {
       try {
          require('../DBconnect.php');
          $db = get_db();
          
          if($_POST['submit'] == 'delete') {
-            $db->query($delete);
+            $db->query("DROP TABLE IF EXISTS project1.spells_by_class, project1.spells, project1.saves_attacks, project1.lengths, project1.classes, project1.schools, project1.sources, project1.users");
          }
          if($_POST['submit'] == 'create') {
-            $db->query($create);
+            $db->query("CREATE TABLE project1.users (
+                  id SERIAL NOT NULL UNIQUE PRIMARY KEY,
+                  username VARCHAR(100) NOT NULL UNIQUE,
+                  password VARCHAR(100) NOT NULL,
+                  name VARCHAR(100) NOT NULL)");
+            $db->query("CREATE TABLE project1.sources (
+                  id SERIAL NOT NULL PRIMARY KEY,
+                  name VARCHAR(100) NOT NULL UNIQUE)");
+            $db->query("CREATE TABLE project1.schools (
+                  id SERIAL NOT NULL UNIQUE PRIMARY KEY,
+                  name VARCHAR(100) NOT NULL UNIQUE)");
+            $db->query("CREATE TABLE project1.classes (
+                  id SERIAL NOT NULL UNIQUE PRIMARY KEY,
+                  name VARCHAR(100) NOT NULL UNIQUE)");
+            $db->query("CREATE TABLE project1.lengths (
+                  id SERIAL NOT NULL UNIQUE PRIMARY KEY,
+                  name VARCHAR(100) NOT NULL UNIQUE)");
+            $db->query("CREATE TABLE project1.saves_attacks (
+                  id SERIAL NOT NULL UNIQUE PRIMARY KEY,
+                  name VARCHAR(100) NOT NULL UNIQUE)");
+            $db->query("CREATE TABLE project1.spells (
+                  id SERIAL NOT NULL UNIQUE PRIMARY KEY,
+                  name VARCHAR(100) NOT NULL UNIQUE,
+                  school_id INT NOT NULL REFERENCES project1.schools(id),
+                  source_id INT NOT NULL REFERENCES project1.sources(id),
+                  casting_time_id INT NOT NULL REFERENCES project1.lengths(id),
+                  duration_id INT NOT NULL REFERENCES project1.lengths(id),
+                  save_id INT REFERENCES project1.saves_attacks(id),
+                  casting_time INT NOT NULL,
+                  duration DECIMAL NOT NULL,
+                  lvl SMALLINT NOT NULL, -- only need numbers -1 thru 10
+                  concentration BOOLEAN NOT NULL,
+                  ritual BOOLEAN NOT NULL,
+                  range INT,
+                  range_type VARCHAR(30),
+                  area VARCHAR(100),
+                  components VARCHAR(7) NOT NULL,
+                  component_desc VARCHAR(100),
+                  consumed BOOLEAN NOT NULL,
+                  description TEXT NOT NULL,
+                  higher_desc TEXT)");
+            $db->query("CREATE TABLE project1.spells_by_class (
+                  id SERIAL NOT NULL UNIQUE PRIMARY KEY,
+                  class_id INT NOT NULL REFERENCES project1.classes(id),
+                  spell_id INT NOT NULL REFERENCES project1.spells(id))");
          }
          if($_POST['submit'] == 'insert') {
             $schools = $db->query("INSERT INTO project1.schools (name) VALUES ('conjuration'), ('necromancy'), ('evocation'), ('abjuration'), ('transmutation'), ('divination'), ('enchantment'), ('illusion')");
