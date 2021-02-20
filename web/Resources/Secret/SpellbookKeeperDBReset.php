64d9e82665ee57887376110f71395b1a85c7a0cf
Suzy <chewypuppielover@yahoo.com>
2021-02-20 13:46:11 -0700

    ,

--- web/Resources/Secret/SpellbookKeeperDBReset.php
+++ web/Resources/Secret/SpellbookKeeperDBReset.php
@@ -65,7 +65,9 @@
             $saves = $db->query("INSERT INTO project1.saves_attacks (name) VALUES ('dex save'), ('str save'), ('con save'), ('int save'), ('wis save'), ('char save'), ('melee'), ('ranged')");
          }
          if($_GET['submit'] == 'json') {
-            $spells = json_decode(file_get_contents('spellData.txt'), true);
+            $json = file_get_contents('./spellData.txt');
+            print_r($json);
+            $spells = json_decode($json, true);
             echo "Spells: ";
             print_r($spells);
             $insert = $db->prepare('INSERT INTO project1.spells (name, school_id, source_id, casting_time_id, duration_id, casting_time, duration, lvl, concentration, ritual, range, range_type, components, component_desc, consumed, description, higher_desc, save_id, area) 
