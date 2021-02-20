0cf49a4c924145ad3066077a31d24dc6d323438b
Suzy <chewypuppielover@yahoo.com>
2021-02-19 19:49:31 -0700

    oops no ifs

--- web/Resources/SpellbookKeeper.js
+++ web/Resources/SpellbookKeeper.js
@@ -32,10 +32,10 @@ function detailFormatter(index, row) {
    var html = []
    html.push(
       '<b>' + row.name + '</b>' + 
-      '</br>\t' + level(row.lvl) + row.school + if(row.ritual)? "(ritual)" + if(row.concentration)? "(concentration)" +
+      '</br>\t' + level(row.lvl) + row.school + (row.ritual)? "(ritual)" + (row.concentration)? "(concentration)" +
       '</br>\tCasting time: ' + row.casting_time + ' ' + row.casting_time_type +
       '</br>\tRange: ' + row.range + ' ' + row.range_type +
-      '</br>\tComponents: ' + row.components + if(row.component_desc)?(' (' + row.component_desc + ')' + if(row.consumed)? "(consumed)") +
+      '</br>\tComponents: ' + row.components + (row.component_desc)?(' (' + row.component_desc + ')' + (row.consumed)? "(consumed)") +
       '</br>\tDuration: ' + row.duration + ' ' + row.duration_type +
       '<p>' + row.description + '</br>' + row.higher_desc + '</p>'
    );
