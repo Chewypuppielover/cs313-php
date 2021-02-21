26676812a7b20081d18dd567d995f49d14b6db92
Suzy <chewypuppielover@yahoo.com>
2021-02-20 23:23:49 -0700

    .

--- web/Resources/SpellbookKeeper.php
+++ web/Resources/SpellbookKeeper.php
@@ -1,16 +1,13 @@
 <?php
-   require('DBconnect.php');
+   require_once('DBconnect.php');
    $db = get_db();
-   
-   $stm = "SELECT sc.name school, so.name source, ct.name casting_time_type, s.name save_attack, spells.name, spells.casting_time, spells.duration, spells.lvl, spells.concentration, spells.ritual, spells.range, spells.range_type, spells.area, spells.components, spells.component_desc, spells.consumed, spells.description, spells.higher_desc, spells.save_id " .
+   try {
+      $res = $db -> query("SELECT sc.name school, so.name source, ct.name casting_time_type, s.name save_attack, spells.name, spells.casting_time, spells.duration, spells.lvl, spells.concentration, spells.ritual, spells.range, spells.range_type, spells.area, spells.components, spells.component_desc, spells.consumed, spells.description, spells.higher_desc, spells.save_id " .
       "FROM project1.spells spells, project1.schools sc, project1.sources so, project1.lengths ct, project1.saves_attacks s " .
       "WHERE spells.school_id = sc.id " .
       "AND spells.source_id = so.id " .
       "AND spells.casting_time_id = ct.id " .
-      "AND spells.save_id = s.id";
-   
-   try {
-      $res = $db -> query($stm);
+      "AND spells.save_id = s.id");
       $all = $res -> fetchAll(PDO::FETCH_ASSOC);
       echo json_encode($all);
    } catch (PDOException $ex) {
